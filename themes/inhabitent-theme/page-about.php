5e59b6c8e073daefea96e0678f03b5da76d2a24e
Kachniss <vopalkova.katerina@gmail.com>
2018-04-28 12:39:30 -0700

    Menu order, add About content

--- themes/inhabitent-theme/page-about.php
+++ themes/inhabitent-theme/page-about.php
@@ -10,9 +10,17 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-            <div class="hero-image">
-                <?php the_post_thumbnail(); ?>
-            </div>
+            <div class="hero-image" style="background-image: linear-gradient(rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)), url(<?php the_post_thumbnail_url(); ?>)">
+				<h1 class="hero-heading"><?php the_title(); ?></h1>
+			</div>
+			
+			<div class="about-page-content">
+				<?php
+					$page = get_page_by_title( get_the_title());
+					$content = apply_filters('the_content', $page->post_content); 
+					echo $content;
+				?>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
